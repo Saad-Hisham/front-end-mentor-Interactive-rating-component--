481d399b0976db775077905139b683a08295cd7e
Saad Hesham <132556058+Saad-Hisham@users.noreply.github.com>
2023-05-22 23:59:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const rate = document.querySelectorAll("span.rate-number");
+const submitButton = document.getElementById("submit");
+const thanksSection = document.querySelector(".thanks-section-card")
+const rateSection = document.querySelector(".rating-section-card")
+const selectedRateSpan = document.querySelector(".select")
+let selectedRate = 0;
+
+for(let i = 0 ; i <rate.length ; i++){
+    
+    rate[i].addEventListener("click",()=>{
+        for(let i = 0 ; i<rate.length ; i++){
+            rate[i].classList.remove("active");
+        }
+        rate[i].classList.add("active")
+        selectedRate = rate[i].innerHTML;
+    })
+
+}
+submitButton.onclick =()=>{
+    thanksSection.style.display="block";
+    rateSection.style.display="none";
+    selectedRateSpan.innerHTML = selectedRate;
+}
